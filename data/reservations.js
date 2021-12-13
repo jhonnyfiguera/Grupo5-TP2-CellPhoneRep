31cea6aa93ccb5f885fdb3996be6abef81149943
jhonnyfiguera <figuerajhonny5@gmail.com>
2021-12-13 18:45:52 -0300

    ultimasCorreciones

--- data/reservations.js
+++ data/reservations.js
@@ -4,78 +4,92 @@ const DATABASE = 'grupo5-cellphone';
 const RESERVATIONSDB = 'reservations';
 
 /**
+ * ------------------------------------------
  * Busqueda de reserva por id
  * @param {id} id
  * @returns reserva
  */
-async function getReservationId(id){
-    const connectiondb = await connection.getConnection();
-    const reservation = await connectiondb
-                        .db(DATABASE)
-                        .collection(RESERVATIONSDB)
-                        .findOne({ _id: new ObjectId(id) });   
-    return reservation;
+async function getReservationId(id) {
+	const connectiondb = await connection.getConnection();
+	const reservation = await connectiondb
+		.db(DATABASE)
+		.collection(RESERVATIONSDB)
+		.findOne({ _id: new ObjectId(id) });
+	return reservation;
 }
 
 /**
+ * ------------------------------------------
  * Busqueda de todas las reservas de un usuario
- * @param {_id} userId 
+ * @param {_id} userId
  * @returns reservas de un usuario
  */
- async function getAllReservationsUser(userId){
-    const connectiondb = await connection.getConnection();
-    const reservations = await connectiondb
-                        .db(DATABASE)
-                        .collection(RESERVATIONSDB)
-                        .find({ 'user._id': new ObjectId(userId) })
-                        .toArray();    
-    return reservations;
+async function getAllReservationsUser(userId) {
+	const connectiondb = await connection.getConnection();
+	const reservations = await connectiondb
+		.db(DATABASE)
+		.collection(RESERVATIONSDB)
+		.find({ 'user._id': new ObjectId(userId) })
+		.toArray();
+	return reservations;
 }
 
 /**
+ * ------------------------------------------
  * Alta de reserva
  * @param {reservation} reservation
  * @returns resultado de Alta de reserva
  */
- async function addReservation(reservation) {
-    const connectiondb = await connection.getConnection();
-    const result = connectiondb
-                .db(DATABASE)
-                .collection(RESERVATIONSDB)
-                .insertOne(reservation);
-    return result;
-  }
-  
+async function addReservation(reservation) {
+	const connectiondb = await connection.getConnection();
+	const result = connectiondb
+    .db(DATABASE)
+    .collection(RESERVATIONSDB)
+    .insertOne(reservation);
+	return result;
+}
+
 /**
+ * ------------------------------------------
  * Cancelar reserva
  * @param {reservation} reservation
  * @returns resultado de cancelar reserva
  */
- async function cancelReservation(reservation) {
-  const connectiondb = await connection.getConnection();
-  const result = connectiondb
-              .db(DATABASE)
-              .collection(RESERVATIONSDB)
-              .updateOne({_id: reservation._id}, {
-                $set: {
-                  state: reservation.state
-                }
-              });
-  return result;
+async function cancelReservation(reservation) {
+	const connectiondb = await connection.getConnection();
+	const result = connectiondb
+		.db(DATABASE)
+		.collection(RESERVATIONSDB)
+		.updateOne(
+			{ _id: reservation._id },
+			{
+				$set: {
+					state: reservation.state,
+				},
+			},
+		);
+	return result;
 }
 
 /**
+ * ------------------------------------------
  * Delete reserva
  * @param {id} id
  * @returns resultado de eliminar reserva
  */
- async function deleteReservation(id) {
-  const connectiondb = await connection.getConnection();
-  const result = connectiondb
-              .db(DATABASE)
-              .collection(RESERVATIONSDB)
-              .deleteOne({ _id: new ObjectId(id) });
-  return result;
+async function deleteReservation(id) {
+	const connectiondb = await connection.getConnection();
+	const result = connectiondb
+		.db(DATABASE)
+		.collection(RESERVATIONSDB)
+		.deleteOne({ _id: new ObjectId(id) });
+	return result;
 }
 
-module.exports = {getReservationId, getAllReservationsUser, addReservation, cancelReservation, deleteReservation};
+module.exports = {
+	getReservationId,
+	getAllReservationsUser,
+	addReservation,
+	cancelReservation,
+	deleteReservation,
+};
--- routes/reservations.js
+++ routes/reservations.js
@@ -1,69 +1,74 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const controller = require("../controllers/reservations");
-const auth = require("../middleware/authentication");
+const controller = require('../controllers/reservations');
+const auth = require('../middleware/authentication');
 
 /*
- * Una reserva por id
- */
-router.get("/:id", auth, async (req, res) => {
-  try {
-    console.log("Reserva por id");
-    res.json(await controller.getReservationId(req.params.id));
-  } catch (error) {
-    res.status(401).send(error.message);
-  }
-});
-
-/*
+ * ----------------------------------------------
  * Todas las reservas de un usuario
  */
-router.get("/user/:userId", auth, async (req, res) => {
-  try {
-    console.log("Reservas por usuario");
-    res.json(await controller.getAllReservationsUser(req.params.userId));
-  } catch (error) {
-    res.status(401).send(error.message);
-  }
+router.get('/user/:userId', auth, async (req, res) => {
+	try {
+		console.log('Reservas por usuario');
+		res.json(await controller.getAllReservationsUser(req.params.userId));
+	} catch (error) {
+		res.status(401).send(error.message);
+	}
 });
 
-/**
+/*
+ * ----------------------------------------------
+ * Una reserva por id de reserva
+ */
+router.get('/:id', auth, async (req, res) => {
+	try {
+		console.log('Reserva por id');
+		res.json(await controller.getReservationId(req.params.id));
+	} catch (error) {
+		res.status(401).send(error.message);
+	}
+});
+
+/*
+ * ----------------------------------------------
  * Alta de reserva
  */
-router.post("/add", async (req, res) => {
-  try {
-    console.log("Alta de reserva. ");
-    const result = await controller.addReservation(req.body);
-    res.send(result);
-  } catch (error) {
-    res.status(401).send(error.message);
-  }
+router.post('/add', async (req, res) => {
+	try {
+		console.log('Alta de reserva. ');
+		const result = await controller.addReservation(req.body);
+		res.send(result);
+	} catch (error) {
+		res.status(401).send(error.message);
+	}
 });
 
-/**
+/*
+ * ----------------------------------------------
  * Cancelación de reserva
  */
- router.put("/cancelReservation/", async (req, res) => {
-  try {
-    console.log("Cancelación de reserva.");
-    const result = await controller.cancelReservation(req.body._id);
-    res.send(result);
-  } catch (error) {
-    res.status(401).send(error.message);
-  }
+router.put('/cancelReservation/', async (req, res) => {
+	try {
+		console.log('Cancelación de reserva.');
+		const result = await controller.cancelReservation(req.body._id);
+		res.send(result);
+	} catch (error) {
+		res.status(401).send(error.message);
+	}
 });
 
-/**
+/*
+ * ----------------------------------------------
  * Delete de reserva
  */
- router.put("/deleteReservation/", async (req, res) => {
-  try {
-    console.log("Delete de reserva.");
-    const result = await controller.deleteReservation(req.body._id);
-    res.send(result);
-  } catch (error) {
-    res.status(401).send(error.message);
-  }
+router.put('/deleteReservation/', async (req, res) => {
+	try {
+		console.log('Delete de reserva.');
+		const result = await controller.deleteReservation(req.body._id);
+		res.send(result);
+	} catch (error) {
+		res.status(401).send(error.message);
+	}
 });
 
 module.exports = router;
